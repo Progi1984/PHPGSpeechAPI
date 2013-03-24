9bcf75212f430bd2d2f8f460473a15dbe847d0b3
Progi1984 <progi1984@gmail.com>
2013-03-24 20:40:30 +0100

    FIXED : Separator (Windows & Linux compatible)

--- samples/sample00.php
+++ samples/sample00.php
@@ -4,4 +4,4 @@
   #ffmpeg.exe -i sample00.mp3 -acodec flac -aq 100 -ar 16000 sample00.flac
   $oPHPGSpeechAPI = new PHPGSpeechAPI();
   $oPHPGSpeechAPI->setLanguage('fr');
-  echo $oPHPGSpeechAPI->recognize(getcwd().'\sample00.flac');
+  echo $oPHPGSpeechAPI->recognize(getcwd().DIRECTORY_SEPARATOR.'sample00.flac');
